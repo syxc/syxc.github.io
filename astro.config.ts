1b0d756ced5a3fb3c29658ad7a811f4049da80e4
nian1 <nian1@duck.com>
2024-06-06 05:59:26 +0800

    Update repo

--- astro.config.ts
+++ astro.config.ts
@@ -9,7 +9,6 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  base: "site",
   integrations: [
     tailwind({
       applyBaseStyles: false,
