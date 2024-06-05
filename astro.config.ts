71f8f471f111624cdba39b4ae518108d0fedc321
syxc <nian1@duck.com>
2024-06-06 06:19:15 +0800

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -9,6 +9,7 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+  base: "syxc.github.io",
   integrations: [
     tailwind({
       applyBaseStyles: false,
