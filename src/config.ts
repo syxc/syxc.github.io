16b1eabeb18156ce1d8c578eda37529717d96d55
nian1 <nian1@duck.com>
2024-06-06 04:39:36 +0800

    Hello astro-paper

---
+++ src/config.ts
@@ -0,0 +1,39 @@
+import type { Site, SocialObjects } from "./types";
+
+export const SITE: Site = {
+  website: "https://syxc.github.io/", // replace this with your deployed domain
+  author: "Sat Naing",
+  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
+  title: "syxc",
+  ogImage: "astropaper-og.jpg",
+  lightAndDarkMode: true,
+  postPerPage: 3,
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+};
+
+export const LOCALE = {
+  lang: "en", // html lang code. Set this empty and default will be "en"
+  langTag: ["en-US"], // BCP 47 Language Tags. Set this empty [] to use the environment default
+} as const;
+
+export const LOGO_IMAGE = {
+  enable: false,
+  svg: true,
+  width: 216,
+  height: 46,
+};
+
+export const SOCIALS: SocialObjects = [
+  {
+    name: "Github",
+    href: "https://github.com/syxc/site",
+    linkTitle: ` ${SITE.title} on Github`,
+    active: true,
+  },
+  {
+    name: "Twitter",
+    href: "https://x.com/syxc",
+    linkTitle: `${SITE.title} on Twitter`,
+    active: true,
+  }
+];
---
+++ src/pages/about.md
@@ -0,0 +1,12 @@
+---
+layout: ../layouts/AboutLayout.astro
+title: "About"
+---
+
+本站由 [Astro](https://astro.build) 和 [astro-paper](https://github.com/satnaing/astro-paper) 提供支持。
+
+AstroPaper 是一款简约、响应迅速且 SEO 友好的 Astro 博客主题。
+
+<div>
+  <img src="/assets/dev.svg" class="sm:w-1/2 mx-auto" alt="coding dev illustration">
+</div>
