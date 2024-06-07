20acaaced7d0140e7e6e34b82b698f38c5dd1986
nian1 <nian1@duck.com>
2024-06-07 18:09:53 +0800

    Update font

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,7 @@ export const SITE: Site = {
 };
 
 export const LOCALE = {
-  lang: "en", // html lang code. Set this empty and default will be "en"
+  lang: "zh", // html lang code. Set this empty and default will be "en"
   langTag: ["en-US"], // BCP 47 Language Tags. Set this empty [] to use the environment default
 } as const;
 
